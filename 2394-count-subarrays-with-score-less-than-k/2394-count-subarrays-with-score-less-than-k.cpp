61475d359a82177f7d25a922a17ea39372bdd9a5
SatyamGupta88 <144547828+SatyamGupta88@users.noreply.github.com>
2023-09-13 13:25:28 +0530

    Time: 101 ms (96.7%), Space: 95.1 MB (27.25%) - LeetHub

--- 2394-count-subarrays-with-score-less-than-k/2394-count-subarrays-with-score-less-than-k.cpp
+++ 2394-count-subarrays-with-score-less-than-k/2394-count-subarrays-with-score-less-than-k.cpp
@@ -4,15 +4,22 @@ public:
     {
         int n = nums.size();
         long long ans = 0;
+        long long product=1;
         long long sum=0;
+        int size=0;
         int i=0;
         for(int j=0;j<n;j++)
         {
             sum+=nums[j];
-            while(sum*(j-i+1)>=k)
+            size = j-i+1;
+            product=(sum*size);
+
+            while(product>=k)
             {
                 sum-=nums[i];
                 i++;
+                size=j-i+1;
+                product=(sum*size);
             }
             ans += j-i+1;
         }
